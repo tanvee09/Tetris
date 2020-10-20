1f7303d2f8d3a7fd00d43982b52d77e8de1bfb5f
divyanshTyagi <divyansh1107@gmail.com>
2020-10-20 22:59:40 +0530

    Added more future pieces

--- client/tetris.js
+++ client/tetris.js
@@ -2,8 +2,15 @@ const cvs = document.getElementById('tetris');
 const ctx = cvs.getContext('2d');
 const scoreElement = document.getElementById('score');
 
-const cvs2 = document.getElementById('nextPiece');
-const ctx2 = cvs2.getContext('2d');
+// instantiating 3 futurePiece Context
+const fp1 = document.getElementById('futurePiece1');
+const ctxfp1 = fp1.getContext('2d');
+
+const fp2 = document.getElementById('futurePiece2');
+const ctxfp2 = fp2.getContext('2d');
+
+const fp3 = document.getElementById('futurePiece3');
+const ctxfp3 = fp3.getContext('2d');
 
 const ROW = 20;
 const COL = 10;
@@ -21,12 +28,30 @@ function drawSquare(x, y, color) {
     // ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
 }
 
-function drawSquareForNextPiece(x, y, color) {
-    ctx2.fillStyle = color;
-    ctx2.fillRect(x * SQ, y * SQ, SQ, SQ);
+// Draws a particular Predicted piece
+function drawSquareForNextPieces(x, y, color, num) {
+    // num specifies which predicted shape it draws
+    if (num == 0) {
+        ctxfp1.fillStyle = color;
+        ctxfp1.fillRect(x * SQ, y * SQ, SQ, SQ);
 
-    ctx2.strokeStyle = 'white';
-    ctx2.strokeRect(x * SQ, y * SQ, SQ, SQ);
+        ctxfp1.strokeStyle = 'white';
+        ctxfp1.strokeRect(x * SQ, y * SQ, SQ, SQ);
+    }
+    if (num == 1) {
+        ctxfp2.fillStyle = color;
+        ctxfp2.fillRect(x * SQ, y * SQ, SQ, SQ);
+
+        ctxfp2.strokeStyle = 'white';
+        ctxfp2.strokeRect(x * SQ, y * SQ, SQ, SQ);
+    }
+    if (num == 2) {
+        ctxfp3.fillStyle = color;
+        ctxfp3.fillRect(x * SQ, y * SQ, SQ, SQ);
+
+        ctxfp3.strokeStyle = 'white';
+        ctxfp3.strokeRect(x * SQ, y * SQ, SQ, SQ);
+    }
 }
 
 
@@ -55,52 +80,66 @@ function drawBoard() {
 
 drawBoard();
 
-function drawNextPieceBoard() {
-    for (var r = 0; r < 4; r++) {
-        for (var c = 0; c < 4; c++) {
-            drawSquareForNextPiece(c, r, VACANT);
-        }
-    }
-}
-
-drawNextPieceBoard();
-
-
-const PIECES = [
-    [Z, 'red'], 
-    [S, 'green'], 
-    [T, 'cyan'], 
-    [O, 'indigo'], 
-    [I, 'yellow'],
-    [L, 'purple'], 
-    [J, 'orange']
-];
-
-
-function randomPiece() {
-    let randomN = Math.floor(Math.random() * PIECES.length);
-    return new Piece(PIECES[randomN][0], PIECES[randomN][1]);
-}
-
-
-//initiate a piece
-let p = randomPiece();
-
-let nextPiece = randomPiece();
-
-function drawNextPiece(nextPiece) {
-    drawNextPieceBoard();
-    for (var r = 0; r < nextPiece.activeTetromino.length; r++) {
-        for (var c = 0; c < nextPiece.activeTetromino.length; c++) {
-            if (nextPiece.activeTetromino[r][c]) {
-                drawSquareForNextPiece(c, r, nextPiece.color);
+function drawAllNextPiecesBoard() {
+    for (var num = 0; num < 3; num++) {
+        for (var r = 0; r < 4; r++) {
+            for (var c = 0; c < 4; c++) {
+                drawSquareForNextPieces(c, r, VACANT, num);
             }
         }
     }
 }
 
-drawNextPiece(nextPiece);
+drawAllNextPiecesBoard();
 
+
+const PIECES = [
+    [Z, 'red'],
+    [S, 'green'],
+    [T, 'cyan'],
+    [O, 'indigo'],
+    [I, 'yellow'],
+    [L, 'purple'],
+    [J, 'orange']
+];
+
+
+//
+
+futurePieces = [];
+
+function generateRandomPieces() {
+    // fill the future pieces array for upto 3 predictions
+    while (futurePieces.length != 3) {
+        let randomN = Math.floor(Math.random() * PIECES.length);
+        futurePieces.push(new Piece(PIECES[randomN][0], PIECES[randomN][1]));
+    }
+}
+
+
+//create the random piece array initiate a piece
+generateRandomPieces();
+
+// sets the first piece up
+let p = futurePieces.shift();
+// fills the remaining futurePieces array which was empty(at last slot) due to above line
+generateRandomPieces();
+
+function drawfuturePieces() {
+    drawAllNextPiecesBoard();
+    for (var num = 0; num < 3; num++) {
+        let nextPiece = futurePieces[num];
+        for (var r = 0; r < nextPiece.activeTetromino.length; r++) {
+            for (var c = 0; c < nextPiece.activeTetromino.length; c++) {
+                if (nextPiece.activeTetromino[r][c]) {
+                    drawSquareForNextPieces(c, r, nextPiece.color, num);
+                }
+            }
+        }
+    }
+}
+
+drawfuturePieces();
 
 // Create a piece
 
@@ -118,7 +157,7 @@ function Piece(Tetromino, color) {
 
 Piece.prototype.fill = function(color) {
     for (var r = 0; r < this.activeTetromino.length; r++) {
-        for (var c = 0;c < this.activeTetromino.length; c++) {
+        for (var c = 0; c < this.activeTetromino.length; c++) {
             if (this.activeTetromino[r][c]) {
                 drawSquare(this.x + c, this.y + r, color);
             }
@@ -137,7 +176,7 @@ Piece.prototype.unDraw = function() {
 
 Piece.prototype.lock = function() {
     for (var r = 0; r < this.activeTetromino.length; r++) {
-        for (var c = 0;c < this.activeTetromino.length; c++) {
+        for (var c = 0; c < this.activeTetromino.length; c++) {
             if (!this.activeTetromino[r][c]) {
                 continue;
             } else if (this.y + r < 0) {
@@ -175,7 +214,7 @@ Piece.prototype.lock = function() {
 
 Piece.prototype.collision = function(x, y, piece) {
     for (var r = 0; r < piece.length; r++) {
-        for (var c = 0;c < piece.length; c++) {
+        for (var c = 0; c < piece.length; c++) {
             if (!piece[r][c]) {
                 continue;
             }
@@ -185,7 +224,7 @@ Piece.prototype.collision = function(x, y, piece) {
                 return true;
             } else if (newY < 0) {
                 continue;
-            } else if (board[newY][newX] !=  VACANT) {
+            } else if (board[newY][newX] != VACANT) {
                 return true;
             }
         }
@@ -201,9 +240,9 @@ Piece.prototype.moveDown = function() {
         this.draw();
     } else {
         this.lock();
-        p = nextPiece;
-        nextPiece = randomPiece();
-        drawNextPiece(nextPiece);
+        p = futurePieces.shift();
+        generateRandomPieces();
+        drawfuturePieces();
     }
 }
 
@@ -228,11 +267,11 @@ Piece.prototype.moveRight = function() {
 
 
 Piece.prototype.rotate = function() {
-    let nextPattern =  this.tetromino[(this.tetrominoN + 1) % this.tetromino.length];
+    let nextPattern = this.tetromino[(this.tetrominoN + 1) % this.tetromino.length];
     let kick = 0;
 
     if (this.collision(0, 0, nextPattern)) {
-        if (this.x > COL/2) {
+        if (this.x > COL / 2) {
             kick = -1;
         } else {
             kick = 1;
@@ -267,7 +306,7 @@ function CONTROL(event) {
     }
 }
 
- 
+
 let dropStart = Date.now();
 let gameOver = false;
 
@@ -284,6 +323,3 @@ function drop() {
 }
 
 drop();
-
-
-
