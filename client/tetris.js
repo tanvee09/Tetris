d54aef68e863a27672b2d0b41166d95bf56fe028
Tanvee Balhara <balharatanvee@gmail.com>
2020-10-21 12:57:31 +0530

    Rectified bug in play/pause

--- client/tetris.js
+++ client/tetris.js
@@ -334,17 +334,19 @@ document.getElementById("newGame").addEventListener('click', startNewGame);
 document.addEventListener("keydown", CONTROL);
 
 function CONTROL(event) {
-    if (event.keyCode == 37) {
-        p.moveLeft();
-        dropStart = Date.now();
-    } else if (event.keyCode == 38) {
-        p.rotate();
-        dropStart = Date.now();
-    } else if (event.keyCode == 39) {
-        p.moveRight();
-        dropStart = Date.now();
-    } else if (event.keyCode == 40) {
-        p.moveDown();
+    if (!paused) {
+        if (event.keyCode == 37) {
+            p.moveLeft();
+            dropStart = Date.now();
+        } else if (event.keyCode == 38) {
+            p.rotate();
+            dropStart = Date.now();
+        } else if (event.keyCode == 39) {
+            p.moveRight();
+            dropStart = Date.now();
+        } else if (event.keyCode == 40) {
+            p.moveDown();
+        }
     }
 }
 
