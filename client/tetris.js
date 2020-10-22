1869bcb0b09ab0b3075e103a87c57873bc68e683
Tanvee Balhara <balharatanvee@gmail.com>
2020-10-22 10:23:34 +0530

    volume control added

--- client/tetris.js
+++ client/tetris.js
@@ -505,3 +505,11 @@ function themeToggle(){
 // function themealt(){
 //     document.body.style.backgroundImage = 'url(./assets/background1.jpg)';
 // }
+
+
+window.SetVolume = function(val) {
+    var player = document.getElementById('song');
+    console.log('Before: ' + player.volume);
+    player.volume = val / 100;
+    console.log('After: ' + player.volume);
+}
