f4c86014c9d8f439c2ce1b57984821353c7f1239
Tanvee Balhara <balharatanvee@gmail.com>
2020-10-24 15:47:36 +0000

    create table query corrected

--- server_and_client/database.sql
+++ server_and_client/database.sql
@@ -2,6 +2,6 @@ CREATE TABLE scores_test(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     score INTEGER NOT NULL CHECK(score >= 0),
-    time_taken INTEGER NOT NULL CHECK (time_taken >= 0)
+    time_taken DOUBLE PRECISION NOT NULL CHECK (time_taken >= 0)
 );
 
